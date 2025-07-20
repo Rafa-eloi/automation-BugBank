f2ff542b7e4522649ef369660a25b1e1dface0f0
Rafael ELoi <rafa.eloi@hotmail.com>
2025-07-20 01:31:08 -0300

    implementação dos cenários de cadastro

--- cypress/e2e/features/hello.feature
+++
@@ -1,5 +0,0 @@
-Feature: Teste inicial
-
-  Scenario: Acessar a página inicial
-    Given que o usuário acessa a página inicial
-    Then a página deve exibir o botão de login
--- cypress/support/step_definitions/hello.steps.js
+++
@@ -1,9 +0,0 @@
-const { Given, Then } = require("@badeball/cypress-cucumber-preprocessor");
-
-Given("que o usuário acessa a página inicial", () => {
-  cy.visit("/");
-});
-
-Then("a página deve exibir o botão de login", () => {
-  cy.contains("Acessar").should("be.visible");
-});
